f0a078f7fbe2414b6c2af183d28ad778b77e6caf
Sahil Jain <sahil.2441@gmail.com>
2016-05-01 18:04:47 -0400

    Adding one more file

--- website_analysis.py
+++ website_analysis.py
@@ -7,7 +7,7 @@ import time
 
 #input_path = '/Users/sahiljain/Google_Drive/SBU/Academics/Spring_16/CSE534/FCN_Project/git_code_self'
 input_path = '~/Desktop/git_code/fcn_project'
-input_file = 'mixed2.txt'
+input_file = 'websites_all.txt'
 
 def analyze():
     file = input_path + '/' + input_file
@@ -16,16 +16,13 @@ def analyze():
     # Go to Resource directory
     os.chdir('/home/sahil/Desktop/git_code/fcn_project')
 	
-    f = ['www.google.com']
-    for line in f:
+    # file = ['www.google.com']
+    for line in file:
         website = line.strip()
         output_file = website + '.txt'
         os.system("/home/sahil/Desktop/Release/chrome "+website+" 2> " + output_file)
-	time.sleep(3)
+    	time.sleep(3)
         os.system("pkill chromium")
-	exit()
-
-#    f.close()
-
+	    # exit()
 if __name__ == '__main__':
     analyze()
