548d16a7f08ac55ee39ff0e99f10894e9fde330c
Sahil Jain <sahil.2441@gmail.com>
2016-05-01 15:51:30 -0400

    First commit

---
+++ website_analysis.py
@@ -0,0 +1,23 @@
+
+import json
+import sys
+import os
+
+
+input_path = '/Users/sahiljain/Google_Drive/SBU/Academics/Spring_16/CSE534/FCN_Project/Colin'
+input_file = 'mixed2.txt'
+
+def analyze():
+    file = input_path + '/' + input_file
+    f=open(file)
+
+    for line in f:
+        website = line.strip()
+        output_file = website + '.txt'
+        os.system("./chromium "+website+" > " + output_file)
+        os.system("pkill chromium")
+
+    f.close()
+
+if __name__ == '__main__':
+    analyze()
